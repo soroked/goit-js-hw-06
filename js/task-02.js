e62e8914faf76b0b0c518d85d418a008a1e8e9a0
Eduard <sorokolietov@gmail.com>
2023-08-10 09:28:54 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// TASK-02
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const ingredientsArray = ingredients.map(element => {
+  const liEl = document.createElement('li');
+  liEl.textContent = element;
+  liEl.classList.add('item');
+  return liEl;
+});
+
+ingredientsEl.append(...ingredientsArray);
